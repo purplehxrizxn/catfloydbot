d45febba0904b10868d82277b9cbefd7034c1bc0
Panza <gabrielpanzeri@live.com>
2020-09-14 13:04:54 -0300

    keys

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const Tweet = new Twitter({
     consumer_key: process.env.BOT_CONSUMER_KEY,
     consumer_secret: process.env.BOT_CONSUMER_SECRET,
     access_token: process.env.BOT_ACCESS_TOKEN,
-    access_token_secret:  process.env.BOT_ACCESS_TOKEN_SECRET
-
+    access_token_secret:  process.env.BOT_ACCESS_TOKEN_SECRET,
 })
 
 function action(event){
